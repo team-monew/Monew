badf1eb7bd1c75c5bb8e61c8398568ff1f4b0b30
MinJi Cho <jam96013@gmail.com>
2025-09-17 20:31:08 +0900

    :construction: Feature : Header 컴포넌트 생성

---
+++ src/Header.tsx
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <header className="w-full h-80px bg-white"></header>;
+}
---
+++ src/HeaderTab.tsx
@@ -0,0 +1,3 @@
+export default function HeaderTab() {
+  return <header className="w-252px h-40px"></header>;
+}
