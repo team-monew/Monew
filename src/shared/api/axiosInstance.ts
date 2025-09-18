8c918eec4a53232add198d108a7d8aa5100d5cf1
MinJi Cho <jam96013@gmail.com>
2025-09-19 03:16:23 +0900

    :wrench: setting : CORS 우회로 프록시 사용

--- src/shared/api/axiosInstance.ts
+++ src/shared/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: "/api/sb/monew/api",
   timeout: 10_000,
   withCredentials: true,
 });
--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,14 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwind(), tsconfigPaths()],
+  server: {
+    proxy: {
+      "/api": {
+        target:
+          "http://sprint-project-1196140422.ap-northeast-2.elb.amazonaws.com",
+        changeOrigin: true,
+        rewrite: (p) => p.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
