31480266113387ef72b66b8ff68bfcfb83e9f3a6
MinJi Cho <jam96013@gmail.com>
2025-09-22 22:59:59 +0900

    :construction: fix: api: NotificationId import 경로 수정

--- src/api/notifications/index.ts
+++ src/api/notifications/index.ts
@@ -1,6 +1,6 @@
 import { http } from "@/lib/http";
 import type * as T from "@/api/notifications/types";
-import type { UserId } from "@/types/ids";
+import type { UserId, NotificationId } from "@/types/ids";
 
 /* 알림 목록 조회 */
 export async function getNotifications(
@@ -28,7 +28,7 @@ export async function checkAllNotifications(
 
 /* 알림 확인(개별) */
 export async function checkNotifications(
-  notificationId: T.NotificationId,
+  notificationId: NotificationId,
   requestUserId: UserId
 ): Promise<void> {
   await http.patch<void>(`/notifications/${notificationId}`, undefined, {
