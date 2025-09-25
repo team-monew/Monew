ce4e7c23509bb8571a8aad21d24b2b494a3b5821
MinJi Cho <jam96013@gmail.com>
2025-09-25 22:05:41 +0900

    ✨ feat(GNB): 인증/미인증 로고 라우팅 분기

--- src/components/common/gnb/PublicHeader.tsx
+++ src/components/common/gnb/PublicHeader.tsx
@@ -1,11 +1,15 @@
+import { Link } from "react-router";
 import Logo from "@/components/common/gnb/Logo";
 import { headerStyle } from "@/components/common/gnb/header.styles";
+import { ROUTES } from "@/shared/constants/routes";
 
 export default function PublicHeader() {
   return (
     <header className={`${headerStyle}`}>
       <div className="flex items-center w-full">
-        <Logo className="md:h-[48px] h-[36px]" />
+        <Link to={ROUTES.ROOT} aria-label="home">
+          <Logo className="md:h-[48px] h-[36px]" />
+        </Link>
       </div>
     </header>
   );
