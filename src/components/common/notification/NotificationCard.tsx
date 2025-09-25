d3272cb2cc56343021947fa2bf30a4b4e3b4346a
MinJi Cho <jam96013@gmail.com>
2025-09-26 05:14:12 +0900

    :construction: design(notificationPanel): UI 초안

---
+++ src/components/common/notification/NotificationCard.tsx
@@ -0,0 +1,8 @@
+import bellIconUrl from "@/assets/icons/bell.svg";
+import heartIconUrl from "@/assets/icons/like-active.svg";
+
+export default function NotificationCard() {
+  return (
+    <div className="w-full max-w-[390px] h-[80px] bg-white border border-gray-200 rounded-xl p-4 gap-2.5"></div>
+  );
+}
