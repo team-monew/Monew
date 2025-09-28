53a64aa36118faa3815446225cba3083ef32c791
M1 <minji@MINJI-MacBook-Air.local>
2025-09-28 23:02:23 +0900

    :sparkles: feat(route): 비로그인 라우트 가드 적용

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,21 @@ import InterestsPage from "@/pages/InterestsPage";
 import ActivitiesPage from "@/pages/ActivitiesPage";
 import NotFound from "@/pages/not-found";
 import TestPage from "@/pages/TestPage";
+import { RequireAuth } from "@/features/auth/guards/RequireAuth";
 
 function App() {
   return (
     <Routes>
-      <Route element={<PrivateLayout />}>
-        <Route path="/articles" element={<ArticlesPage />} />
-        <Route path="/interests" element={<InterestsPage />} />
-        <Route path="/activities" element={<ActivitiesPage />} />
+      {/* 보호 라우트: 로그인 필요 */}
+      <Route element={<RequireAuth />}>
+        <Route element={<PrivateLayout />}>
+          <Route path="/articles" element={<ArticlesPage />} />
+          <Route path="/interests" element={<InterestsPage />} />
+          <Route path="/activities" element={<ActivitiesPage />} />
+        </Route>
       </Route>
 
+      {/* 게스트 전용 */}
       <Route element={<PublicLayout />}>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
