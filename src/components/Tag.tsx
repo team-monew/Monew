76ff89846891c70422c5b2c0c6260479fca73643
송형진 <sori4606@naver.com>
2025-09-29 17:13:36 +0900

    :recycle: refactor : Tag 컴포넌트 onClick props 제거

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,22 +1,14 @@
 interface TagProps {
   label: string;
-  onClick?: () => void;
   onRemove?: () => void;
 }
 
-export default function Tag({ label, onClick, onRemove }: TagProps) {
+export default function Tag({ label, onRemove }: TagProps) {
   return (
     <div
       className={`w-fit h-8 px-2 py-1 rounded-lg bg-slate-100 flex items-center gap-1`}
     >
-      <p
-        className={`text-16-m text-slate-500 ${
-          onClick ? "cursor-pointer" : ""
-        }`}
-        onClick={onClick}
-      >
-        {label}
-      </p>
+      <p className={`text-16-m text-slate-500`}>{label}</p>
       {onRemove && (
         <button
           type="button"
