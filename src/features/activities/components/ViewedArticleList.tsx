4bd3a5c74d70f4a9d76e95274dba943696e46e50
송형진 <sori4606@naver.com>
2025-10-01 14:13:31 +0900

    :wrench: fix : vercel 배포오류 해결

--- src/features/activities/components/ViewedArticleList.tsx
+++ src/features/activities/components/ViewedArticleList.tsx
@@ -2,12 +2,17 @@ import { useUserActivitiesList } from "@/features/activities/hooks/useUserActivi
 import NewsCard from "@/features/articles/components/NewsCard";
 import EmptyState from "@/components/EmptyState";
 import Skeleton from "@/components/Skeleton";
+import type { ArticleListItem } from "@/api/articles/types";
+import useArticleDetailModal from "@/shared/hooks/useArticleDetailModal";
+import ArticleDetailModal from "@/components/modal/ArticleDetailModal";
 
 export default function ViewedArticleList() {
   const { items, error, loading, empty } = useUserActivitiesList(
     "viewedArticles",
     5,
   );
+
+  const { isOpen, openModal, onClose, initialData } = useArticleDetailModal();
 
   if (error) {
     return <p className="text-14-r text-error">{error}</p>;
@@ -20,20 +25,33 @@ export default function ViewedArticleList() {
   }
 
   return (
-    <ul className="flex flex-col gap-4 divide-y divide-gray-300">
-      {items.map((a) => (
-        <li key={a.id}>
-          <NewsCard
-            title={a.articleTitle}
-            summary={a.articleSummary}
-            source={a.source}
-            sourceUrl={a.sourceUrl}
-            publishDate={a.articlePublishedDate}
-            viewCount={a.articleViewCount}
-            commentCount={a.articleCommentCount}
-          />
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className="flex flex-col gap-4 divide-y divide-gray-300">
+        {items.map((a) => {
+          const article: ArticleListItem = {
+            id: a.id,
+            title: a.articleTitle,
+            summary: a.articleSummary,
+            source: a.source,
+            sourceUrl: a.sourceUrl,
+            publishDate: a.articlePublishedDate,
+            viewCount: a.articleViewCount,
+            commentCount: a.articleCommentCount,
+            viewedByMe: true,
+          };
+
+          return (
+            <li key={a.id}>
+              <NewsCard article={article} onClick={() => openModal(article)} />
+            </li>
+          );
+        })}
+      </ul>
+      <ArticleDetailModal
+        isOpen={isOpen}
+        onClose={onClose}
+        data={initialData}
+      />
+    </>
   );
 }
