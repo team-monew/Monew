0e18613d08bfac48f6259d85651dc6db7931da04
MinJi Cho <jam96013@gmail.com>
2025-10-02 13:18:50 +0900

    chore(layout): 중복 overflow-auto 코드 제거

--- src/app/layouts/PrivateLayout.tsx
+++ src/app/layouts/PrivateLayout.tsx
@@ -4,13 +4,13 @@ import Footer from "@/shared/components/Footer";
 
 export default function PrivateLayout() {
   return (
-    <div className="min-h-dvh flex flex-col space-y-[60px]">
+    <div className="min-h-dvh flex flex-col gap-[60px]">
       <div className="sticky top-0 z-30">
         <PrivateHeader />
       </div>
 
       <div className="flex-1 overflow-x-auto">
-        <main className="flex-1 min-w-[1280px] px-4 overflow-x-auto">
+        <main className="flex-1 min-w-[1280px] px-4">
           <Outlet />
         </main>
       </div>
